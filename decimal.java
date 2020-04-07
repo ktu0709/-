a86c6761a34a6fbc47b55f1b3eaf28e9a651c470
ktu0709 <44633204+ktu0709@users.noreply.github.com>
2020-04-07 11:40:16 +0900

    Update decimal.java

--- decimal.java
+++ decimal.java
@@ -8,15 +8,13 @@ public class Hello {
     System.out.println("2부터 100까지 소수:");
     for(x=1;x<100;x++) {
     	y=2;
-    while(y<x)
-    {
+    while(y<x){
     	if(x%y==0)
     		break;
     	else
     		y++;
     }
-    if(x==y)
-    {
+    if(x==y){
     	System.out.println(y);
     }
    }
